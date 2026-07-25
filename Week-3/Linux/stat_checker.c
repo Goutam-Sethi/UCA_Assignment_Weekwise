e2398990f797242abbfd4ac2c7e295ddb01dc58a
Goutam Sethi <goutamsethi5050@gmail.com>
2026-07-25 20:24:51 +0530

    Stat_Checker Code Added

--- Week-3/Linux/stat_checker.c
+++ Week-3/Linux/stat_checker.c
@@ -17,22 +17,38 @@ int main(int argc, char *argv[]) {
   }
 
   char *type;
+
   switch (sb.st_mode & S_IFMT) {
-    case S_IFREG:  type = "Regular File";    break;
-    case S_IFDIR:  type = "Directory";       break;
-    case S_IFCHR:  type = "Character Device"; break;
-    case S_IFBLK:  type = "Block Device";    break;
-    case S_IFIFO:  type = "FIFO/Pipe";       break;
-    case S_IFLNK:  type = "Symbolic Link";   break;
-    case S_IFSOCK: type = "Socket";          break;
-    default:       type = "Unknown";
+    case S_IFREG:
+      type = "Regular File";
+      break;
+    case S_IFDIR:
+      type = "Directory";
+      break;
+    case S_IFCHR:
+      type = "Character Device";
+      break;
+    case S_IFBLK:
+      type = "Block Device";
+      break;
+    case S_IFIFO:
+      type = "FIFO/Pipe";
+      break;
+    case S_IFLNK:
+      type = "Symbolic Link";
+      break;
+    case S_IFSOCK:
+      type = "Socket";
+      break;
+    default:
+      type = "Unknown";
   }
 
   printf("=== File Attributes for: %s ===\n", argv[1]);
   printf("File Type:            %s\n", type);
   printf("Permissions:          0%o\n", sb.st_mode & 0777);
-  printf("File Size:            %lld bytes\n", (long long) sb.st_size);
-  printf("Hard Links Count:     %ld\n", (long) sb.st_nlink);
+  printf("File Size:            %lld bytes\n", (long long)sb.st_size);
+  printf("Hard Links Count:     %ld\n", (long)sb.st_nlink);
   printf("Owner (UID):          %d\n", sb.st_uid);
   printf("Group (GID):          %d\n", sb.st_gid);
   printf("Last Access Time:     %s", ctime(&sb.st_atime));
